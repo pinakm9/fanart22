84d3041031337345e5fb71b3e48848ea53a92060
pinakm9 <pinakm9@gmail.com>
2022-06-29 15:42:12 +0530

    broken html

---
+++ script.js
@@ -0,0 +1,7 @@
+$('#v11').hover(function toggleControls() {
+    if (this.hasAttribute("controls")) {
+        this.removeAttribute("controls")
+    } else {
+        this.setAttribute("controls", "controls")
+    }
+})
