b11d20a000da951528b1e390f9db400fdd5e615c
pinakm9 <pinakm9@gmail.com>
2022-06-30 00:53:04 +0530

    final

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-$('#v11').hover(function toggleControls() {
-    if (this.hasAttribute("controls")) {
-        this.removeAttribute("controls")
-    } else {
-        this.setAttribute("controls", "controls")
-    }
-})
+(function($){
+
+    
+	$('#v11, #v12, #v13').hover(function toggleControls() {
+		if (this.hasAttribute("controls")) {
+			this.removeAttribute("controls")
+		} else {
+			this.setAttribute("controls", "controls")
+		}
+	});
+
+})(jQuery);
